5144bc0a001694b9916082423e7b45cd64b37182
ilabstaff <ilabstaff@172.16.116.68>
2019-05-20 17:45:21 -0700

    taco superpowers

---
+++ src/superpowers.java
@@ -0,0 +1,24 @@
+
+public class superpowers {
+	public String name;
+	
+	public String superpower;
+	
+	void name(String name) {
+		name=name;
+	
+	}
+	String name() {
+	return name;
+	}	
+	void superpower(String s) {
+		superpower=s;
+	}
+	String superpower() {
+		return superpower;
+		}
+public static void main(String[] args) {
+	
+
+}
+}
---
+++ src/taco.java
@@ -0,0 +1,28 @@
+
+public class taco {
+	
+	private  String meat;
+	private String sauce;
+	
+	void setMeat(String meat) {
+		meat = meat;
+	}
+	
+	String getMeat() {
+		return meat;
+	}
+	
+	void setSauce(String s) {
+		sauce = s;
+	}
+	
+	String getsauce() {
+		return sauce;
+	}
+
+	
+public static void main(String[] args) {
+	
+	
+}
+}
