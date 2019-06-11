3029b4938ebbc79888f3cfcdc56d5b52580a25bc
ilabstaff <ilabstaff@172.16.116.79>
2019-06-10 17:51:47 -0700

    handy smurf

--- src/_03_smurf/handy_smurf.java
+++ src/_03_smurf/handy_smurf.java
@@ -3,14 +3,23 @@ package _03_smurf;
 public class handy_smurf {
 public static void main(String[] args) {
 	Smurf Papa= new Smurf("Papa");
-	//Smurf Smurfette= new Smurf("Smurfette");
+	Smurf Smurfette= new Smurf("Smurfette");
 	
 	String getName= Papa.getName();
 	System.out.println(getName);
 	
+	String getName1= Smurfette.getName();
+	System.out.println(getName1);
+	
+	
 	Papa.eat();
+	
+	Smurfette.eat();
 	
 	String getHatColor=Papa.getHatColor();
 	System.out.println(getHatColor);
+	
+	String isGirlOrBoy=Smurfette.getHatColor();
+	System.out.println(isGirlOrBoy);
 }
 }
