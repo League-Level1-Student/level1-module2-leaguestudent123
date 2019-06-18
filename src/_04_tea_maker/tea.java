e5ac43b952880f906831f83d024000d4c0270d52
ilabstaff <ilabstaff@172.16.116.77>
2019-06-17 17:40:21 -0700

    tea

--- src/_04_tea_maker/tea.java
+++ src/_04_tea_maker/tea.java
@@ -1,5 +1,10 @@
 package _04_tea_maker;
 
 public class tea {
-
+public static void main(String[] args) {
+	TeaBag TeaBag=new TeaBag("TeaBag");
+	
+	String flavor= TeaBag.getFlavor();
+	flavor.Water(true);
+}
 }
