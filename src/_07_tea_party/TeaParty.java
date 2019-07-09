7cbdc924f3154d968eb1fc7ce4d3008f0dab8ea2
ilabstaff <ilabstaff@172.16.116.71>
2019-07-08 17:41:25 -0700

    tea party tea partytest

---
+++ src/_07_tea_party/TeaParty.java
@@ -0,0 +1,21 @@
+package _07_tea_party;
+
+import javax.swing.JOptionPane;
+
+public class TeaParty {
+    public String welcome(String austen, boolean isWoman, boolean isKnighted) {
+    	String res = "";
+    	
+    	if( isWoman == true) {
+    	
+    	
+    	}
+    	
+    	
+    	return res;
+    	
+    	 
+   
+    	
+    }
+}
